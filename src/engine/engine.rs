e6c8dd4dd312aad7890e2a58a5442e586b594418
ComicalCache <57747276+ComicalCache@users.noreply.github.com>
2025-07-23 01:10:16 +0200

    WIP

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -261,8 +261,7 @@ impl Engine {
                 // Inverse result due to symmetry.
                 if let Some(eval) = new_eval
                     && -eval > alpha
-                    && beta != i64::MIN + 1
-                    && beta - alpha > 1
+                    && -eval < beta
                 {
                     new_eval = self.pvs(new_board, &None, -beta, -alpha, depth - 1);
                 }
