2a5fd3bf60fc17a20523ab8d3970e4c621c842b8
ComicalCache <57747276+ComicalCache@users.noreply.github.com>
2025-07-28 01:19:19 +0200

    Version

--- src/uci/uci_sender.rs
+++ src/uci/uci_sender.rs
@@ -9,7 +9,12 @@ pub fn ready_ok() {
 
 /// id.
 pub fn id() {
-    println!("id name Rxh7+ V1.2");
+    println!(
+        "id name Rxh7+ V{}.{}.{}",
+        env!("CARGO_PKG_VERSION_MAJOR"),
+        env!("CARGO_PKG_VERSION_MINOR"),
+        env!("CARGO_PKG_VERSION_PATCH")
+    );
     println!("id author ComicalCache");
     println!("uciok");
 }
