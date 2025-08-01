ab3db4b7e4a758f118ea71f11b566577b1493983
ComicalCache <57747276+ComicalCache@users.noreply.github.com>
2025-08-01 22:22:16 +0200

    rev8

--- src/engine/time_control.rs
+++ src/engine/time_control.rs
@@ -156,7 +156,8 @@ impl Engine {
             if time > safety_margin {
                 self.search.hard_move_time = Some(Duration::from_millis(hard_move_time));
                 // Set soft time limit to be 75% of the hard time limit.
-                self.search.soft_move_time = Some(Duration::from_millis(15 * hard_move_time / 20));
+                // self.search.soft_move_time = Some(Duration::from_millis(15 * hard_move_time / 20));
+                self.search.soft_move_time = Some(Duration::from_millis(hard_move_time));
             }
         }
     }
